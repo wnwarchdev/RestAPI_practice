fb6b2aa12e625284aa89dc48fb1402cb3aa9a5e1
Wojtek Nowak <billnewmann@gmail.com>
2020-06-20 18:07:05 +0100

    Add put, delete, random and id endpoints

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 
 const db = [
-    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+    { id: 1, author: 'John Doe', text: 'Worth every coin!' },
+    { id: 2, author: 'Amanda Doe', text: 'Make you happy.' },
     { id: 3, author: 'Fox Mulder', text: 'The truth is out there.' },
     { id: 4, author: 'Dana Scully', text: 'Sure. Fine. Whatever.' }, 
     { id: 5, author: 'Walter Skinner', text: 'Are we done?' },
     { id: 6, author: 'Little Green Man', text: 'We come in peace!' },
   ];
-
+  //console.log(db.length);
+  //console.log(Math.floor(Math.random()*db.length));
+  //console.log(db)
+  //console.log(db[2])
+  //console.log(db[Math.floor(Math.random()*db.length)])
+  
 
 const app = express();
 
@@ -22,7 +27,33 @@ app.get('/testimonials', (req, res) => {
     res.json(db);
   });
 
+app.get('/testimonials/random', (req, res) => {
+    res.json(db[Math.floor(Math.random()*db.length)]);
+  });  //ciekawe ze nie działa pod :id....
 
+app.get('/testimonials/:id', (req, res) => {
+    res.json(db.filter(item => item.id == req.params.id));
+  });
+
+app.delete('/testimonials/:id', (req, res) => {
+    const object = db.filter(item => item.id == req.params.id);
+    db.splice(db.indexOf(object[0]), 1);
+    res.json({message: 'OK, deleted'});
+  });
+
+app.put('/testimonials/:id', (req, res) => {
+    const payload = {
+        id: req.params.id, 
+        author: 'placeholderAuthor', 
+        text: 'placeholderText'
+      }
+    db.push(payload);  
+    res.json({message: 'OK, updated'});
+  });
+
+  app.use((req, res) => {
+    res.status(404).render('error');
+  })
 
 
 app.listen(8000, () => {
