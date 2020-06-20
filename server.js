897ac9dd1d8188c51132c663ff3396a0bad8b27a
Wojtek Nowak <billnewmann@gmail.com>
2020-06-20 14:58:40 +0100

    Add middleware

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
