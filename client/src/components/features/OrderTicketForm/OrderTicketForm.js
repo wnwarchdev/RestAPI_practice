cf8b7fbfc65c3c1244ef895128333897b69730f0
Wojtek Nowak <billnewmann@gmail.com>
2020-07-24 22:32:56 +0100

    Fix?

--- client/src/components/features/OrderTicketForm/OrderTicketForm.js
+++ client/src/components/features/OrderTicketForm/OrderTicketForm.js
@@ -39,7 +39,7 @@ class OrderTicketForm extends React.Component {
 
   submitForm = async (e) => {
     const { order } = this.state;
-    const { addSeat } = this.props; //loadSeats
+    const { addSeat, loadSeats } = this.props; //loadSeats
 
     e.preventDefault();
 
@@ -54,7 +54,7 @@ class OrderTicketForm extends React.Component {
         },
         isError: false,
       });
-      //loadSeats();
+      loadSeats();
     } else {
       this.setState({ isError: true });
     }
