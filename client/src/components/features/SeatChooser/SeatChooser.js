d0b83996e7cf5c4401ead8aee074a041870f5811
Wojtek Nowak <billnewmann@gmail.com>
2020-07-25 00:11:49 +0100

    Fix2

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -10,7 +10,7 @@ class SeatChooser extends React.Component {
   componentDidMount() {
     const { loadSeats, loadSeatsSocket } = this.props; //loadSeats
     //this.refresh = setInterval(() => loadSeats(), 2*60*1000);
-    this.socket = io.connect(process.env.ENV_NODE || 'http://localhost:8000');
+    this.socket = io.connect((process.env.NODE_ENV === 'production') ? '' : 'localhost:8000');
     loadSeats()
     this.socket.on('seatsUpdated', (seats) => {
       loadSeatsSocket(seats)
