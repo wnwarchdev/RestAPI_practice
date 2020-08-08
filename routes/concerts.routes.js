86a65b618023b4da3f02961d8629221920d1b2a4
Wojtek Nowak <billnewmann@gmail.com>
2020-08-08 16:39:07 +0100

    Add new endpoints

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -9,4 +9,9 @@ router.post('/concerts', ConcertController.post);
 router.put('/concerts/:id', ConcertController.put);
 router.delete('/concerts/:id', ConcertController.delete);
 
+router.get('/concerts/performer/:performer', ConcertController.getPerformer);
+router.get('/concerts/genre/:genre', ConcertController.getGenre);
+router.get('/concerts/price/:price_min/:price_max', ConcertController.getPrice);
+router.get('/concerts/price/day/:day', ConcertController.getDay);
+
 module.exports = router;
