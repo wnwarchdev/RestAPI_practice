8fbdbfd3a3c70dc94e20815b9695d8feda852890
Wojtek Nowak <billnewmann@gmail.com>
2020-08-11 19:59:26 +0100

    Add helmet middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(helmet());
 
 
 
